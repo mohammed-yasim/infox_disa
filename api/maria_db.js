7bbdc3a679b64ba7f7a521fcda738d5547423a21
mohammed-yasim <mohammedyasim.edkm@gmail.com>
2021-11-15 22:26:44 +0530

    Production Method

--- api/maria_db.js
+++ api/maria_db.js
@@ -32,7 +32,7 @@ Object.defineProperty(exports, "infox_sequlize", {
 var _sequelize = require("sequelize");
 
 var infox_db = new _sequelize.Sequelize('u947217984_infox', 'u947217984_infox', '9hHQKOTx3gKH', {
-  host: 'sql336.main-hosting.eu',
+  host: '45.84.204.52',
   dialect: 'mariadb',
   pool: {
     max: 150,
--- api_src/api/maria_db.js
+++ api_src/api/maria_db.js
@@ -1,6 +1,6 @@
 import { Sequelize, Model, DataTypes, Op } from 'sequelize';
 const infox_db = new Sequelize('u947217984_infox', 'u947217984_infox', '9hHQKOTx3gKH', {
-    host: 'sql336.main-hosting.eu',
+    host: '45.84.204.52',
     dialect: 'mariadb',
     pool: {
         max: 150,
