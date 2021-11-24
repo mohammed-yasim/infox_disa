6fc3cd5e7c71223db2113dd6b7aaf9858a48a0a7
mohammed-yasim <mohammedyasim.edkm@gmail.com>
2021-11-24 20:30:33 +0530

    Production Method

--- infox/src/infox_app/modules/quotation/preview.js
+++ infox/src/infox_app/modules/quotation/preview.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { infoxAPI } from "../../etc/api";
+import $ from 'jquery';
+import image from './file.webp'
 class QuotationPreview extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             quotation: null,
-            no: ''
+            no: '',
+            data: []
 
         }
     }
@@ -15,7 +18,7 @@ class QuotationPreview extends React.Component {
             (response) => {
                 this.setState({
                     quotation: JSON.parse(response.data.blob),
-                    no: response.data.no
+                    data: response.data
                 });
             });
     }
@@ -23,21 +26,39 @@ class QuotationPreview extends React.Component {
         this.props.history.goBack();
     }
     componentDidMount() {
+        var script = '<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
+        $('head').append(script);
         this.loadData();
+    }
+    print = () => {
+        let element = document.getElementById("print-area")
+        let opt = {
+            margin: 0.1,
+            filename: this.state.data.name,
+            image: { type: 'jpeg', quality: 0.98 },
+            pagebreak: { mode: ['css', 'legacy'] },
+            html2canvas: { scale: 1 },
+            jsPDF: { unit: 'in', format: 'A4', orientation: 'portrait' }
+        };
+        window.html2pdf().set(opt).from(element).save();
     }
     render() {
         let quotation = this.state.quotation
         return (<>
             <div className="d-print-none">
                 <h4><button onClick={this.goBack}
-                    className="btn btn-link btn-lg"><i className="fa fa-arrow-left"></i></button>Quotation Preview <button onClick={()=>{
+                    className="btn btn-link btn-lg"><i className="fa fa-arrow-left"></i></button>Quotation Preview <button onClick={() => {
                         window.print();
-                    }} className="btn btn-sm btn-primary shadow-sm float-right"><i className="fa fa-print"></i> Print</button></h4>
+                    }} className="btn btn-sm btn-primary shadow-sm float-right m-1"><i className="fa fa-print"></i> Print</button>
+                    <button onClick={() => {
+                        this.print();
+                    }} className="btn btn-sm btn-primary shadow-sm float-right m-1"><i className="fa fa-download"></i>DD </button>
+                </h4>
             </div>
             {quotation !== null ?
-                <div>
+                <div id="print-area" className="p-5">
                     <div className="text-center">
-                        <img alt="header" width="100%" src="https://www.adobe.com/express/create/media_1319c302e16d1d18f8dc4a3e887778ef8959c12a6.jpeg?width=2000&format=webply&optimize=medium" className="img-fluid" />
+                        <img alt="header" width="100%" src={image} className="img-fluid" />
                     </div>
                     <div className="row mt-5">
                         <div className="col-md-8">
@@ -55,7 +76,7 @@ class QuotationPreview extends React.Component {
                             <table className="table table-bordered table-sm">
                                 <tbody>
                                     <tr>
-                                        <th>Quotation No. </th><td className="text-center">{this.state.no}</td>
+                                        <th>Quotation No. </th><td className="text-center">{this.state.data.no}</td>
                                     </tr>
                                     <tr>
                                         <th>Date </th><td className="text-center">{quotation.date}</td>
@@ -93,7 +114,7 @@ class QuotationPreview extends React.Component {
                         </tbody>
                         <tfoot className="text-right">
                             <tr>
-                                <th  colSpan={4}> Sub Total : </th><th>&#8377; {quotation.total} </th>
+                                <th colSpan={4}> Sub Total : </th><th>&#8377; {quotation.total} </th>
                             </tr>
                             <tr>
                                 <th colSpan={4}> Discount(%): </th><th>{quotation.discount}</th>
@@ -105,8 +126,8 @@ class QuotationPreview extends React.Component {
                     </table>
                     <div>
                         <p>
-                        WE HOPE YOU WILL FIND OUR ABOVE OFFER SUITABLE AND FAVOUR US WITH YOUR VALUED
-                        ORDERS.</p>
+                            WE HOPE YOU WILL FIND OUR ABOVE OFFER SUITABLE AND FAVOUR US WITH YOUR VALUED
+                            ORDERS.</p>
                         <p className="text-left">THANK YOU FOR YOUR BUSINESS!</p>
                     </div>
                 </div> : null}
