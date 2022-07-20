179f8bbff41edeb3eabd54d49b540b5b977d9fec
mohammed-yasim <mohammedyasim.edkm@gmail.com>
2022-07-20 13:05:19 +0530

    Production Method

--- api_src/api/index.js
+++ api_src/api/index.js
@@ -223,13 +223,13 @@ API_Router.post('/clock_', Middleware, Scheduler, (req, res) => {
                             });
                         }).catch(
                             (err) => {
-                                res.status(403).json(err);
+                                res.status(403).send(err);
                                 console.log(2, err)
                             })
                     }
                 }).catch(
                     (err) => {
-                        res.status(403).json(err);
+                        res.status(403).send(err);
                         console.log(1, err)
                     })
         })
